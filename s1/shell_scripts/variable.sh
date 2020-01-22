4e1659cf273d500295c3cb55e8e93f414681be62
lilone <lionelgatibelza@gmail.com>
2020-01-22 15:27:52 +0100

    correct the two first exercices

--- s1/shell_scripts/variable.sh
+++ s1/shell_scripts/variable.sh
@@ -4,4 +4,6 @@ BIRTHDATE="Jan 1, 2000"
 Presents=10
 BIRTHDAY=`date -j -f '%b %d, %Y' "$BIRTHDATE" +%A`
 
-echo "I was born on $BIRTHDAY the $BIRTHDATE";
+echo "BIRTHDATE is correct, it is $BIRTHDATE"
+echo "I have received $Presents presents"
+echo "I was born on a $BIRTHDAY"
