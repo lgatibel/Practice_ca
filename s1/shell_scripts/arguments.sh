53686d8c817c8ff5a88ddd696de353a445362723
lilone <lionelgatibelza@gmail.com>
2020-01-22 14:49:21 +0100

    first push

---
+++ s1/shell_scripts/arguments.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+arg1="The first argument is : $1\n";
+arg2="The Second argument is : $2\n";
+arg3="The third argument is : $3\n";
+arg4="The fourth argument is : $4\n";
+
+
+echo $arg1;
+echo $arg2;
+echo $arg3;
+echo $arg4;
