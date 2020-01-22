327bc5db5cc205079c525539e1aeb249a61ff89c
lilone <lionelgatibelza@gmail.com>
2020-01-23 00:20:46 +0100

    update programs

--- s1/programs/majuscule.php
+++ s1/programs/majuscule.php
@@ -1,21 +1,25 @@
 <?php
+// check if an argument has been enter
 if (count($argv) < 2){
     echo "Saisissez un argument.";
+// check if the number of args is equal to two
 } elseif (count($argv) != 2) {
     echo "Saissisez un seul argument.";
 } else {
+    // initialise sentence with the argument
     $sentence = $argv[1];
 
     $modifiedSentence = '';
     $numberOfChar = strlen($sentence);
-    echo $numberOfChar . PHP_EOL;
     for($index = 0; $index < $numberOfChar; $index++) {
+        // Transform even char to Uppercase
         if ($index % 2 == 1) {
             $modifiedSentence .= strtoupper($sentence[$index]);
+        // Transform odd char to lowercase
         } else {
             $modifiedSentence .= strtolower($sentence[$index]);
         }
     }
+    //print result sentence
     echo $modifiedSentence;
 }
-echo PHP_EOL . "END OF PROGRAMME.";
