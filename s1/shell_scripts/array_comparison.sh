996df8eeee5ccc276a8816aaa5ee12f15a919ae1
lilone <lionelgatibelza@gmail.com>
2020-01-23 00:04:31 +0100

    finish the last shel exercice array_comparison

--- s1/shell_scripts/array_comparison.sh
+++ s1/shell_scripts/array_comparison.sh
@@ -1,1 +1,59 @@
 #!/bin/bash
+
+a=(3 5 8 10 6)
+b=(6 5 4 12)
+c=(14 7 5 7)
+
+# echo ${a[@]}
+# echo ${b[@]}
+# echo ${c[@]}
+
+
+#initialise all variable for comparison
+indexA=0
+indexB=0
+
+lengthA=${#a[@]}
+lengthB=${#b[@]}
+
+aAndB=()
+indexAandB=0
+
+#coparison of array a And b
+while [[ $indexA -lt $lengthA ]]; do
+    indexB=0
+    while [[ $indexB -lt $lengthB ]]; do
+        if [[ ${a[$indexA]} -eq  ${b[$indexB]} ]]; then
+            aAndB[$indexAandB]="${a[$indexA]}"
+            indexAandB=$(($indexAAndB + 1))
+        fi
+        indexB=$(($indexB + 1))
+    done
+    indexA=$(($indexA + 1))
+done
+
+#initialise all variable for comparison
+aAndBAndC=()
+
+indexAAndB=0
+indexAAndBAndC=0
+indexC=0
+
+lenghtAAndB=${#aAndB[@]}
+lengthC=${#c[@]}
+
+#coparison of array a And b And c
+while [[ $indexAAndB -lt $lenghtAAndB ]]; do
+    indexC=0
+    while [[ $indexC -lt $lengthC ]]; do
+        if [[ ${aAndB[$indexAAndB]} -eq  ${c[$indexC]} ]]; then
+            aAndBAndC[$indexAAndBAndC]="${aAndB[$indexAAndB]}"
+            indexAandBAndC=$(($indexAAndB + 1))
+        fi
+        indexC=$(($indexC + 1))
+    done
+    indexAAndB=$(($indexAAndB + 1))
+done
+
+#print result
+echo "${aAndBAndC[@]}"
